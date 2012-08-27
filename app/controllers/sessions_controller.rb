66a47a3b33638ad8996414178feea4bca398c907
Fernando Munoz <munozfj@hotmail.com>
2012-08-27 09:28:08 -0300

    Fin de edicion,actualizacion, index y eliminacion de usuarios

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
-      flash.now[:error] = 'Combinacion invalida de E-mail y password'
+      flash.now[:error] = 'Combinacion invalida de e-mail y password'
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,66 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+  
   def show
     @user = User.find(params[:id])
   end
   
-  def new
-    @user = User.new
+   def index
+    @users = User.paginate(page: params[:page])
   end
   
-   def create
+  def new
+     @user = User.new
+  end
+  
+  def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
+      flash[:success] = "Bienvenido a Control-8!"
       redirect_to @user
-      flash[:success] = "Bienvenido a Control8"
     else
       render 'new'
     end
   end
   
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Perfil actualizado."
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Usuario se ha eliminado."
+    redirect_to users_url
+  end
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Por favor, ingrese nuevamente."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
 end
